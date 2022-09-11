69fbf2baf557ed61502b838efee4095bb8803456
DJ Drakos <dianajodrakos@gmail.com>
2022-09-11 14:24:48 -0700

    render nav list based on json data and hold selection in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import data from './data.json';
 
 function App() {
+
+  const timeframes = Object.keys(data[0].timeframes);
+  const [timeframe, setTimeframe] = useState('daily');
+
+  function handleClick(value) {
+    setTimeframe(value);
+  }
+
   return (
-    <div className="App">
+    <div className="App"> 
       <header>
         <section className="profile">
           <img src="./images/image-jeremy.png" alt="Jeremy Robson" />
@@ -12,9 +22,7 @@ function App() {
 
         <nav>
           <ul>
-            <li>Daily</li>
-            <li>Weekly</li>
-            <li>Monthly</li>
+            {timeframes.map(item => <li key={item} onClick={() => handleClick(item)}>{item}</li>)}
           </ul>
         </nav>
       </header>
@@ -22,10 +30,10 @@ function App() {
       <main>
         <section className='activity-card work'>
         <div className="activity-card-data">
-          <h2>Work</h2>
+          <h2>{data[0].title}</h2>
           <img src="./images/icon-ellipsis.svg" alt="More Information" />
-          <p>5hrs</p>
-          <p>Previous - 7hrs</p>
+          <p>{data[0].timeframes.daily.current} hrs</p>
+          <p>Yesterday - {data[0].timeframes.daily.previous} hrs</p>
         </div>
         </section>
 
@@ -75,7 +83,7 @@ function App() {
         </section>
       </main>
       
-      <footer class="attribution">
+      <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
         Coded by <a href="https://github.com/dj-drakos">DJ Drakos</a>.
       </footer>
