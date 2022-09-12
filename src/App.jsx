daf8cbc955e7a9cf4a2e408133b3dc5d6e21e575
DJ Drakos <dianajodrakos@gmail.com>
2022-09-11 17:00:56 -0700

    refactor activity list to render based on json data, create functions to format timeframes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,16 @@ function App() {
   const timeframes = Object.keys(data[0].timeframes);
   const [timeframe, setTimeframe] = useState('daily');
 
-  function handleClick(value) {
-    setTimeframe(value);
+  function formatTime(time) {
+    if(time === 1) return `${time} hr`;
+    else return `${time} hrs`
+  }
+
+  function formatPrevDescription() {
+    if(timeframe === 'daily') return 'Yesterday';
+    else if(timeframe === 'weekly') return 'Last Week';
+    else if(timeframe === 'monthly') return 'Last Month';
+    else return 'Last Period';
   }
 
   return (
@@ -22,65 +30,22 @@ function App() {
 
         <nav>
           <ul>
-            {timeframes.map(item => <li key={item} onClick={() => handleClick(item)}>{item}</li>)}
+            {timeframes.map(item => <li key={item} onClick={() => setTimeframe(item)}>{item}</li>)}
           </ul>
         </nav>
       </header>
 
       <main>
-        <section className='activity-card work'>
-        <div className="activity-card-data">
-          <h2>{data[0].title}</h2>
-          <img src="./images/icon-ellipsis.svg" alt="More Information" />
-          <p>{data[0].timeframes.daily.current} hrs</p>
-          <p>Yesterday - {data[0].timeframes.daily.previous} hrs</p>
-        </div>
-        </section>
-
-        <section className='activity-card play'>
-          <div className="activity-card-data">
-            <h2>Play</h2>
-            <img src="./images/icon-ellipsis.svg" alt="More Information" />
-            <p>5hrs</p>
-            <p>Previous - 7hrs</p>
-          </div>
-        </section>
-
-        <section className='activity-card study'>
-          <div className="activity-card-data">
-            <h2>Study</h2>
-            <img src="./images/icon-ellipsis.svg" alt="More Information" />
-            <p> 5hrs</p>
-            <p>Previous - 7hrs</p>
-          </div>
-        </section>
-
-        <section className='activity-card exercise'>
-          <div className="activity-card-data">
-            <h2 >Exercise</h2>
-            <img src="./images/icon-ellipsis.svg" alt="More Information" />
-            <p>5hrs</p>
-            <p>Previous - 7hrs</p>
-          </div>
-        </section>
-
-        <section className='activity-card social'>
-          <div className="activity-card-data">
-            <h2 >Social</h2>
-            <img src="./images/icon-ellipsis.svg" alt="More Information" />
-            <p>5hrs</p>
-            <p>Previous - 7hrs</p>
-          </div>
-        </section>
-
-        <section className='activity-card self-care'>
+        {data.map(item => (
+          <section key={item.title} className={`activity-card ${item.title.toLowerCase().replace(' ', '-')}`}>
             <div className="activity-card-data">
-            <h2 >Self Care</h2>
-            <img src="./images/icon-ellipsis.svg" alt="More Information" />
-            <p>5hrs</p>
-            <p>Previous - 7hrs</p>
-          </div>
-        </section>
+              <h2>{item.title}</h2>
+              <img src="./images/icon-ellipsis.svg" alt="More Information" />
+              <p>{formatTime(item.timeframes[timeframe].current)}</p>
+              <p>{formatPrevDescription()} - {formatTime(item.timeframes[timeframe].previous)}</p>
+            </div>
+          </section>
+        ))}
       </main>
       
       <footer className="attribution">
